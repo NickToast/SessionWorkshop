cccec43a2b17d274a39a69049d353ca43e2a183e
NickToast <nicholastangsouvanh@gmail.com>
2023-09-13 01:39:41 -0700

    Update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,6 +36,7 @@ public class HomeController : Controller
             return RedirectToAction("Index");
         }
         return View();
+        
     }
 
     //passing in a parameter from dashboard (+1, -1, x2, rand)
